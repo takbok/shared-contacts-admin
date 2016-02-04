37ad3627017d9a641876f9c5288c1e70a817d740
Prakhar Kumar <hacker.eth@gmail.com>
2016-02-05 00:30:11 +0530

    Fix formatting

--- SharedContactsDemo_test.go
+++ SharedContactsDemo_test.go
@@ -14,8 +14,8 @@
 package demo
 
 import (
-        "testing"
-        "encoding/xml"
+	"encoding/xml"
+	"testing"
 )
 
 const testFeed = `<?xml version='1.0' encoding='UTF-8'?>
@@ -53,31 +53,65 @@ const testFeed = `<?xml version='1.0' encoding='UTF-8'?>
 </feed>`
 
 func TestFeed(t *testing.T) {
-        var feed Feed
+	var feed Feed
+
 	if err := xml.Unmarshal([]byte(testFeed), &feed); err != nil {
-                t.Errorf("unmarshal feed: %v", err)
+		t.Errorf("unmarshal feed: %v", err)
 		return
 	}
 
-        if n := len(feed.Entry); n != 2 { t.Errorf("len(feed.Entry) != 2"); return }
-        
-        e1 := &feed.Entry[0]
-        e2 := &feed.Entry[1]
+	if n := len(feed.Entry); n != 2 {
+		t.Errorf("len(feed.Entry) != 2")
+		return
+	}
 
-        if s := e1.Name.GivenName; s != "Contact"    { t.Errorf("e1.Name.GivenName != Contact (%v)", s);  return }
-        if s := e1.Name.FamilyName; s != "One"       { t.Errorf("e1.Name.FamilyName != One (%v)", s); return }
-        if s := e2.Name.GivenName; s != "Contact"    { t.Errorf("e2.Name.GivenName != Contact (%v)", s);  return }
-        if s := e2.Name.FamilyName; s != "Two"       { t.Errorf("e2.Name.FamilyName != Two (%v)", s); return }
+	e1 := &feed.Entry[0]
+	e2 := &feed.Entry[1]
 
-        if n := len(e1.Email); n != 1                { t.Errorf("len(e1.Email) != 1 (%v)", n); return }
-        if n := len(e2.Email); n != 1                { t.Errorf("len(e2.Email) != 1 (%v)", n); return }
+	if s := e1.Name.GivenName; s != "Contact" {
+		t.Errorf("e1.Name.GivenName != Contact (%v)", s)
+		return
+	}
+	if s := e1.Name.FamilyName; s != "One" {
+		t.Errorf("e1.Name.FamilyName != One (%v)", s)
+		return
+	}
+	if s := e2.Name.GivenName; s != "Contact" {
+		t.Errorf("e2.Name.GivenName != Contact (%v)", s)
+		return
+	}
+	if s := e2.Name.FamilyName; s != "Two" {
+		t.Errorf("e2.Name.FamilyName != Two (%v)", s)
+		return
+	}
 
-        em1 := e1.Email[0]
-        em2 := e2.Email[0]
+	if n := len(e1.Email); n != 1 {
+		t.Errorf("len(e1.Email) != 1 (%v)", n)
+		return
+	}
+	if n := len(e2.Email); n != 1 {
+		t.Errorf("len(e2.Email) != 1 (%v)", n)
+		return
+	}
 
-        if s := em1.Address; s != "contact1@example.com" { t.Errorf("em1.Address != contact1@example.com (%v)", s); return }
-        if s := em2.Address; s != "contact2@example.com" { t.Errorf("em2.Address != contact2@example.com (%v)", s); return }
+	em1 := e1.Email[0]
+	em2 := e2.Email[0]
 
-        if b := em1.Primary; !b { t.Errorf("!em1.Primary (%v)", b); return }
-        if b := em2.Primary; !b { t.Errorf("!em2.Primary (%v)", b); return }
+	if s := em1.Address; s != "contact1@example.com" {
+		t.Errorf("em1.Address != contact1@example.com (%v)", s)
+		return
+	}
+	if s := em2.Address; s != "contact2@example.com" {
+		t.Errorf("em2.Address != contact2@example.com (%v)", s)
+		return
+	}
+
+	if b := em1.Primary; !b {
+		t.Errorf("!em1.Primary (%v)", b)
+		return
+	}
+	if b := em2.Primary; !b {
+		t.Errorf("!em2.Primary (%v)", b)
+		return
+	}
 }
