28b186b79615ef6e1520d1a0e7ff544ae017f15c
Prakhar Kumar <hacker.eth@gmail.com>
2016-02-11 06:08:10 +0530

    Introduce a unified function to get the contacts URL with the correct parameters

--- api.go
+++ api.go
@@ -11,8 +11,23 @@ import (
 	newappengine "google.golang.org/appengine"
 )
 
+const contactsFeedUrl = `https://www.google.com/m8/feeds/contacts/%s/%s?v=3.0&start-index=%d&max-results=%d&alt=%s`
+
+func getContactsFeedUrl(feedtype, domain string, full bool, start, number int) string {
+	var projection = `full`
+	if !full {
+		projection = `thin`
+	}
+
+	if feedtype != `rss` && feedtype != `json` {
+		feedtype = `atom`
+	}
+
+	return fmt.Sprintf(contactsFeedUrl, domain, projection, start, number, feedtype)
+}
+
 func loadAllContacts(domain string, client *http.Client, context appengine.Context) (buf *bytes.Buffer) {
-	res, err := client.Get(fmt.Sprintf(`https://www.google.com/m8/feeds/contacts/%s/full?v=3.0&max-results=100`, domain))
+	res, err := client.Get(getContactsFeedUrl(`atom`, domain, false, 1, 100))
 	if err != nil {
 		context.Errorf("get: %v", err)
 		return
